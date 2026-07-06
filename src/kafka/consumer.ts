ff4640518d7ff1ff627508a871e4e1ba2e4778e1
root <root@HOSTCOIN-2853-4220.com>
2026-07-06 22:28:43 +0530

    consume monitoring events from Kafka

--- src/kafka/consumer.ts
+++ src/kafka/consumer.ts
@@ -14,4 +14,30 @@ export const connectConsumer = async () => {
   await consumer.connect();
 
   console.log("✅ Logger Consumer Connected");
+
+  await consumer.subscribe({
+    topic: process.env.KAFKA_TOPIC!,
+    fromBeginning: false,
+  });
+
+  console.log(`📌 Subscribed to ${process.env.KAFKA_TOPIC}`);
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      if (!message.value) return;
+
+      const event = JSON.parse(message.value.toString());
+
+      console.log("\n==============================");
+      console.log("📥 EVENT RECEIVED");
+      console.log("==============================");
+
+      console.log("Topic      :", topic);
+      console.log("Partition  :", partition);
+      console.log("Offset     :", message.offset);
+
+      console.log("Payload");
+      console.log(event);
+    },
+  });
 };
